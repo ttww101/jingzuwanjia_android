779adf7a4d9901bfd32ba2a466e47b34e717e91b
wen <gn01150313@hotmail.com>
2019-06-14 09:54:26 +0800

    竟足玩家

---
+++ app/src/main/java/com/ccaaii/shenghuotong/utils/ExtraKey.java
@@ -0,0 +1,9 @@
+package com.ccaaii.shenghuotong.utils;
+
+import com.ga.sdk.constant.BaseExtraKey;
+
+public class ExtraKey extends BaseExtraKey {
+
+    public static final String APPID = "riYrNEkPE8pMhvJvz9fWhmBT-gzGzoHsz";
+    public static final String APPKEY = "0xR7Hi7DWF0Edzs1yqiotrOy";
+}
